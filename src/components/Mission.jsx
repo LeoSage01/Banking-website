1efc0f6219fa91f47e751b7a620b71486133caae
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-06-17 17:08:15 +0100

    Update Mission.jsx

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,33 +1,4 @@
-// import Button from "./button";
-// import Tom from "../assets/manisolated.jpg"
-
-// const Mission = () => {
-//     return (
-//         <section className="p-4">
-//             <div className="rounded-lg grid grid-cols-2 gap-6 bg-purple-600 p-6 ">
-//                 <div className="">
-//                     <img className="h-auto rounded-lg max-w-full" src={Tom} alt="" />
-//                 </div>
-
-//                 <div className="px-8 py-2 text-white mr-2 flex flex-col justify-between space-y-3 ">
-//                     <h3 className=" text-xl font-bold w-34">Discover the mission & story behind our company</h3>
-
-//                     <p className="text-xs">
-//                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus turpis vitae ex vestibulum efficitur. Donec euismod diam eu turpis curshonct dolor tortor. Duis lacinia
-//                     </p>
-
-//                     <Button />
-                   
-//                 </div>
-//             </div>
-//         </section>
-//     );
-// }
-
-// export default Mission; 
-
-
-import Button from "./button";
+import Button from "./Button";
 import Tom from "../assets/manisolated.jpg";
 
 const Mission = () => {
